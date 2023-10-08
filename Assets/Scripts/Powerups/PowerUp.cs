b0ae37a4ba17ff8169984d146e02599fd4f2981f
Zeldruck <felix.jouannneau@gmail.com>
2023-10-08 15:42:55 -0400

    Power ups

--- Assets/Scripts/Powerups/PowerUp.cs
+++ Assets/Scripts/Powerups/PowerUp.cs
@@ -7,6 +7,8 @@ public abstract class PowerUp : MonoBehaviour
     protected abstract void GivePlayerPowerUp(Player player);
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("Player")) return;
+        
         GivePlayerPowerUp(other.GetComponent<Player>());
         gameObject.SetActive(false);
     }
